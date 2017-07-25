b05f30bb934efcf0e97eea22949e59f9f3780762
Michał Matczuk <michal@scylladb.com>
2017-07-25 14:30:13 +0200

    doc update

--- doc.go
+++ doc.go
@@ -1,26 +1,38 @@
 // Package gocqlx is a gocql extension, similar to what sqlx is to database/sql.
 //
-// It provides a new type that seamlessly wraps gocql.Iter and provide
-// convenience methods which are useful in the development of database driven
-// applications.  None of the underlying gocql.Iter methods are changed.
-// Instead all extended behavior is implemented through new methods defined on
-// wrapper type.
+// It contains wrappers over gocql types that provide convenience methods which
+// are useful in the development of database driven applications.  Under the
+// hood it uses sqlx/reflectx package so sqlx models will also work with gocqlx.
 //
-// The wrapper type enables you to bind iterator row into a struct. Under the
-// hood it uses sqlx/reflectx package, models / structs working whit sqlx will
-// also work with gocqlx.
-//
-// Example, read all items for a given id
+// Example, read all rows into a slice
 //
 //     var v []*Item
 //     if err := gocqlx.Select(&v, session.Query(`SELECT * FROM items WHERE id = ?`, id)); err != nil {
 //         log.Fatal("select failed", err)
 //     }
 //
-// Example, read first item for a given id
+// Example, read a single row into a struct
 //
 //     var v Item
 //     if err := gocqlx.Get(&v, session.Query(`SELECT * FROM items WHERE id = ?`, id)); err != nil {
 //         log.Fatal("get failed", err)
 //     }
+//
+// Example, bind named query parameters from a struct or map
+//
+//     stmt, names, err := gocqlx.CompileNamedQuery([]byte("INSERT INTO items (id, name) VALUES (:id, :name)"))
+//     if err != nil {
+//         t.Fatal("compile:", err)
+//     }
+//     q := gocqlx.Queryx{
+//         Query: session.Query(stmt),
+//         Names: names,
+//     }
+//     if err := q.BindStruct(&Item{"id", "name"}); err != nil {
+//         t.Fatal("bind:", err)
+//     }
+//     if err := q.Query.Exec(); err != nil {
+//         log.Fatal("get failed", err)
+//     }
+//
 package gocqlx
