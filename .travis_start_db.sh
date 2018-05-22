444d07da6ab841ff59a4685cecd63f19f7239ba5
Michał Matczuk <michal@scylladb.com>
2018-05-22 15:15:55 +0200

    travis: scylla update to 2.1

--- .travis_start_db.sh
+++ .travis_start_db.sh
@@ -4,10 +4,10 @@ set -e
 case ${DB} in
 
 scylla)
-    echo "deb [arch=amd64] http://s3.amazonaws.com/downloads.scylladb.com/deb/ubuntu trusty scylladb-1.7/multiverse" | sudo tee -a /etc/apt/sources.list > /dev/null
+    sudo curl -o /etc/apt/sources.list.d/scylla.list -L http://repositories.scylladb.com/scylla/repo/20fc70b18261bf832cf8e0733a27979c/ubuntu/scylladb-2.1-trusty.list
     sudo apt-get -qq update
     sudo apt-get install -y --allow-unauthenticated scylla-server
-    sudo /usr/bin/scylla --options-file /etc/scylla/scylla.yaml ${SCYLLA_OPTS} ${SCYLLA_OPTS_LOG} &
+    sudo /usr/bin/scylla --options-file /etc/scylla/scylla.yaml ${SCYLLA_OPTS} &
     ;;
 
 cassandra)
