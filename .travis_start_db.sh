83026b240e470c4394d78934f56301223e95a4ca
Michał Matczuk <michal@scylladb.com>
2018-05-24 13:34:02 +0200

    travis: scylla log to file

--- .travis_start_db.sh
+++ .travis_start_db.sh
@@ -7,7 +7,7 @@ scylla)
     sudo curl -o /etc/apt/sources.list.d/scylla.list -L http://repositories.scylladb.com/scylla/repo/20fc70b18261bf832cf8e0733a27979c/ubuntu/scylladb-2.1-trusty.list
     sudo apt-get -qq update
     sudo apt-get install -y --allow-unauthenticated scylla-server
-    sudo /usr/bin/scylla --options-file /etc/scylla/scylla.yaml ${SCYLLA_OPTS} &
+    sudo /usr/bin/scylla --options-file /etc/scylla/scylla.yaml ${SCYLLA_OPTS} &> /tmp/scylla.log &
     ;;
 
 cassandra)
