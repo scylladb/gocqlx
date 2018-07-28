2093c0cfa35461295e95376d073ae5f1227b06c9
Michał Matczuk <michal@scylladb.com>
2018-07-28 06:41:33 +0200

    makefile: switched to golangci-lint

--- reflectx/reflect.go
+++ reflectx/reflect.go
@@ -167,7 +167,7 @@ func (m *Mapper) FieldsByName(v reflect.Value, names []string) []reflect.Value {
 // to a struct.  Returns empty int slice for each name not found.
 func (m *Mapper) TraversalsByName(t reflect.Type, names []string) [][]int {
 	r := make([][]int, 0, len(names))
-	m.TraversalsByNameFunc(t, names, func(_ int, i []int) error {
+	m.TraversalsByNameFunc(t, names, func(_ int, i []int) error { // nolint
 		if i == nil {
 			r = append(r, []int{})
 		} else {
