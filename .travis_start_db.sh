eddedae353c2219fee099431b1b6312e937c7670
Henrik Johansson <henrik@scylladb.com>
2019-05-31 19:25:05 +0200

    scylla: bump version to 3.0

--- .travis_start_db.sh
+++ .travis_start_db.sh
@@ -4,7 +4,7 @@ set -e
 case ${DB} in
 
 scylla)
-    sudo curl -o /etc/apt/sources.list.d/scylla.list -L http://repositories.scylladb.com/scylla/repo/20fc70b18261bf832cf8e0733a27979c/ubuntu/scylladb-2.1-trusty.list
+    sudo curl -o /etc/apt/sources.list.d/scylla.list -L http://repositories.scylladb.com/scylla/repo/20fc70b18261bf832cf8e0733a27979c/ubuntu/scylladb-3.0-trusty.list
     sudo apt-get -qq update
     sudo apt-get install -y --allow-unauthenticated scylla-server
     sudo /usr/bin/scylla --options-file /etc/scylla/scylla.yaml ${SCYLLA_OPTS} &> /tmp/scylla.log &
