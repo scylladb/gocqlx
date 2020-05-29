d76ea6c678ff4d8612b703a98510afba97a13577
xiang.wang <xiang.wang@kaiostech.com>
2020-05-29 18:47:02 +0800

    Specify explicitly that use gocql.Session.NewBatch instead of the deprecated BatchBuilder.

--- qb/batch.go
+++ qb/batch.go
@@ -24,7 +24,7 @@ type BatchBuilder struct {
 
 // Batch returns a new BatchBuilder.
 //
-// Deprecated: use session.NewBatch() instead
+// Deprecated: Please use gocql.Session.NewBatch() instead.
 func Batch() *BatchBuilder {
 	return &BatchBuilder{}
 }
