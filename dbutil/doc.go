69f6f201f262416d881000f45cf1758310770596
Michał Matczuk <michal@scylladb.com>
2020-12-02 14:08:49 +0100

    dbutil: RewriteTable, generalize table.RewriteRows to copy from source table to destination table with row transformation

---
+++ dbutil/doc.go
@@ -0,0 +1,6 @@
+// Copyright (C) 2017 ScyllaDB
+// Use of this source code is governed by a ALv2-style
+// license that can be found in the LICENSE file.
+
+// Package dbutil provides various utilities built on top of core gocqlx modules.
+package dbutil
